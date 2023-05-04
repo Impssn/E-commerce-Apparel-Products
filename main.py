678b990946e24de670b46035e68f9b761f28dc75
Sathya Narayanan <79907441+Impssn@users.noreply.github.com>
2023-05-04 20:24:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import pickle
 import tensorflow
 from tensorflow.keras.preprocessing import image
 from tensorflow.keras.layers import GlobalMaxPooling2D
-from tensorflow.keras.applications.resnet50 import ResNet50,preprocess_input
+from tensorflow.keras.applications.resnet50 import ResNet50,preprocess_input  
 from sklearn.neighbors import NearestNeighbors
 from numpy.linalg import norm
 
