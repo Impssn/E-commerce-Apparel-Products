08abb9191571a6f14a8ec31680c8e2a77c247c21
Sathya Narayanan <79907441+Impssn@users.noreply.github.com>
2023-05-04 20:25:07 +0530

    Update test.py

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import numpy as np
 from numpy.linalg import norm
 from tensorflow.keras.preprocessing import image
 from tensorflow.keras.layers import GlobalMaxPooling2D
-from tensorflow.keras.applications.resnet50 import ResNet50,preprocess_input
+from tensorflow.keras.applications.resnet50 import ResNet50,preprocess_input   
 from sklearn.neighbors import NearestNeighbors
 import cv2
 
